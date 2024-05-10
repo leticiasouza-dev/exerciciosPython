6c6a1cd5d0d25246da47e6458e1f8ea41674f649
leticiaSouzaDev <lec667121@gmail.com>
2024-05-10 15:14:07 -0300

    feat: adicionando ex1 da aula 1, semana 9

--- Semana_9/aula_1/ex1.py
+++ Semana_9/aula_1/ex1.py
@@ -1,5 +1,63 @@
 #1.	Crie um programa que ajude você a jogar “Stop” de nomes de pessoas.
 #Passo a passo do exercício:
 #tilizar a estrutura de seleção do Python, com o comando elif, para cada opção de letra.
+letra = input("Digite uma letra:");
+
+
+if letra == 'a':
+    print("Ana")
+elif letra == 'b':
+    print("Bianca")
+elif letra == 'c':
+    print("Carol")
+elif letra == 'd':
+    print("Daniela")
+elif letra == 'e':
+    print("Elisa")
+elif letra == 'f':
+    print("Francisca")
+elif letra == 'g':
+    print("Geovana")
+elif letra == 'h':
+    print("Heloisa")
+elif letra == 'i':
+    print("Iorrana")
+elif letra == 'j':
+    print("Janaina")
+elif letra == 'k':
+    print("Kamile")
+elif letra == 'l':
+    print("Letícia")
+elif letra == 'm':
+    print("Melissa")
+elif letra == 'n':
+    print("Nair")
+elif letra == 'o':
+    print("Olivia")
+elif letra == 'p':
+    print("Pricilla")
+elif letra == 'q':
+    print("Quelidônia")
+elif letra == 'r':
+    print("Romilda")
+elif letra == 's':
+    print("Sabrina")
+elif letra == 't':
+    print("Taina")
+elif letra == 'u':
+    print("Uyara")
+elif letra == 'v':
+    print("Vanessa")
+elif letra == 'w':
+    print("Walquíria ")
+elif letra == 'x':
+    print("Xerazade ")
+elif letra == 'y':
+    print("Yorrana")
+elif letra == 'z':
+    print("Zaya")
+else:
+    print("Letra inválida. Digite uma letra do alfabeto.")
+
 
 
