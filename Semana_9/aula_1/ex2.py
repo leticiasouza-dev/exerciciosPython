8e06f429a7933499ce05add60b1b543a23d799d5
leticiaSouzaDev <lec667121@gmail.com>
2024-05-10 15:07:35 -0300

    feat: add ex2 aula 1 da semana 9

---
+++ Semana_9/aula_1/ex2.py
@@ -0,0 +1,58 @@
+letra = input('Digite uma letra do alfabeto: ')
+
+letraMaiuscula = letra.upper()
+
+if letra == 'a':
+    print("Abacaxi")
+elif letra == 'b':
+    print("Banana")
+elif letra == 'c':
+    print("Cereja")
+elif letra == 'd':
+    print("Damasco")
+elif letra == 'e':
+    print("Embu")
+elif letra == 'f':
+    print("Framboesa")
+elif letra == 'g':
+    print("Goiaba")
+elif letra == 'h':
+    print("Hortelã")
+elif letra == 'i':
+    print("Iogurte")
+elif letra == 'j':
+    print("Jabuticaba")
+elif letra == 'k':
+    print("Kiwi")
+elif letra == 'l':
+    print("Laranja")
+elif letra == 'm':
+    print("Melancia")
+elif letra == 'n':
+    print("Nectarina")
+elif letra == 'o':
+    print("Oxicoco")
+elif letra == 'p':
+    print("Pêssego")
+elif letra == 'q':
+    print("Quiuí")
+elif letra == 'r':
+    print("Romã")
+elif letra == 's':
+    print("Sapoti")
+elif letra == 't':
+    print("Tangerina")
+elif letra == 'u':
+    print("Uva")
+elif letra == 'v':
+    print("Vassoura")
+elif letra == 'w':
+    print("Wampi")
+elif letra == 'x':
+    print("Xixá")
+elif letra == 'y':
+    print("Yuzu")
+elif letra == 'z':
+    print("Zimbro")
+else:
+    print("Letra inválida. Digite uma letra do alfabeto.")
